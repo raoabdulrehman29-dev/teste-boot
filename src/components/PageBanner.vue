f3c9c97e3ea5e8da1234bac8465e3be6e3b260ec
raoabdulrehman29-dev <raoabdulrehman29@gmail.com>
2026-07-31 13:01:23 +0500

    page banner fix

--- src/components/PageBanner.vue
+++ src/components/PageBanner.vue
@@ -3,7 +3,6 @@
     <div
       class="banner relative isolate rounded-3xl rounded-bl-[2rem] rounded-br-[2rem] mt-3 mb-10 xl:mb-10 h-[280px] md:h-[400px] lg:h-[540px] lg:w-full"
     >
-    <HeroScene3D />
       <!-- Top Left Decoration -->
       <div class="absolute left-0 top-0 z-20 hidden lg:block rounded-3xl">
         <div class="relative">
